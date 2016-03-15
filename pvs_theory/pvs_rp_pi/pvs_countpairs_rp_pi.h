cda05e92ca763b3fa2901ea6ff5d68205b64847d
antbbn <antbbn@gmail.com>
2016-03-15 21:01:38 +1100

    First Commit for Pairwise Velocity Statistics. pvs_rp_pi working.

---
+++ pvs_theory/pvs_rp_pi/pvs_countpairs_rp_pi.h
@@ -0,0 +1,45 @@
+/* File: pvs_countpairs_rp_pi.h */
+/*
+		This file is a part of the Corrfunc package
+		Copyright (C) 2015-- Manodeep Sinha (manodeep@gmail.com)
+		License: MIT LICENSE. See LICENSE file under the top-level
+		directory at https://github.com/manodeep/Corrfunc/
+*/
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "function_precision.h" //for definition of DOUBLE
+#include <inttypes.h> //for uint64_t
+
+//define the results structure
+typedef struct{
+	uint64_t *npairs;
+	DOUBLE *rupp;
+	DOUBLE *rpavg;
+        DOUBLE *vpavg;
+        DOUBLE *spavg;
+        DOUBLE *vtavg;
+        DOUBLE *stavg;
+	DOUBLE pimax;
+	int nbin;
+	int npibin;
+} results_pvs_countpairs_rp_pi;
+
+results_pvs_countpairs_rp_pi * pvs_countpairs_rp_pi(const int64_t ND1, const DOUBLE *X1, const DOUBLE *Y1, const DOUBLE *Z1,const DOUBLE *VX1, const DOUBLE *VY1, const DOUBLE *VZ1,
+											const int64_t ND2, const DOUBLE *X2, const DOUBLE *Y2, const DOUBLE *Z2,const DOUBLE *VX2, const DOUBLE *VY2, const DOUBLE *VZ2,
+#ifdef USE_OMP
+											const int numthreads,
+#endif
+											const int autocorr,
+											const char *binfile,
+											const double pimax)  __attribute__((warn_unused_result));
+
+void free_results_pvs_rp_pi(results_pvs_countpairs_rp_pi **results);
+
+#ifdef __cplusplus
+}
+#endif
